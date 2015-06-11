ec9a2c70a4d0f10a30280d2851b2aa7142860639
Matthew Batema <MBatema@trustwave.com>
2015-06-10 22:59:18 -0700

    Yay, packages!

--- init.el
+++ init.el
@@ -1,1 +1,22 @@
 ;;; I'm a dog using emacs. HALP! I don't know what I'm doing.
+
+;; Yay packages!
+(require 'package)
+;; Do I need these other repos? Who knows!
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ;("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
+(package-initialize)
+
+;; Shamelessly stolen!
+(defun require-package (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not becall
+re-downloaded in order to locate PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (package-install package)
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
