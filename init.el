5b08012030439bac9d1250ef20e50f9a0db624d8
Matthew Batema <MBatema@trustwave.com>
2015-08-10 15:13:54 -0700

    Enable `flyspell-prog-mode` for all `prog-mode`s.

--- init.el
+++ init.el
@@ -135,6 +135,7 @@ re-downloaded in order to locate PACKAGE."
 
 (require-package 'flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
+(add-hook 'prog-mode-hook 'flyspell-prog-mode)
 
 (require-package 'flycheck-pos-tip)
 (with-eval-after-load 'flycheck
