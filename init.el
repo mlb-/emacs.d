95c116cac3e7abe17f55093544403f1fc350743b
Matthew Batema <mbatema@sensity.com>
2016-02-25 14:49:37 -0800

    Whitespace. Awesome.

--- init.el
+++ init.el
@@ -245,3 +245,12 @@
 (use-package n4js
   :commands n4js-start
   :config (setq n4js-cli-arguments '("-port" "5555")))
+
+(use-package whitespace
+  :init
+  (dolist (hook '(prog-mode-hook text-mode-hook))
+    (add-hook hook #'whitespace-mode))
+  (add-hook 'before-save-hook #'whitespace-cleanup)
+  :config
+  (setq whitespace-line-column 80) ;; limit line length
+  (setq whitespace-style '(face tabs empty trailing lines-tail)))
