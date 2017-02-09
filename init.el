b58e78b7e1652955e26d42e6f4db5673229fbdc2
Matthew Batema <mbatema@sensity.com>
2017-02-08 16:30:37 -0800

    Try `smartparens` in JS mode.

--- init.el
+++ init.el
@@ -181,20 +181,16 @@
 (use-package evil
   :disabled t)
 
-(defun electric-pair ()
-  "If at end of line, insert character pair without surrounding spaces.
-    Otherwise, just insert the typed character."
-  (interactive)
-  (if (eolp) (let (parens-require-spaces) (insert-pair)) (self-insert-command 1)))
+(use-package smartparens-config
+  :ensure smartparens
+  :commands turn-on-smartparens-strict-mode)
+
 (use-package js2-mode
   :mode "\\.js$"
   :config (setq js2-basic-offset 4)
 
   (add-hook 'js2-mode-hook
-            (lambda ()
-              (define-key js2-mode-map "(" 'electric-pair)
-              (define-key js2-mode-map "{" 'electric-pair)
-              (define-key js2-mode-map "[" 'electric-pair)))
+            'turn-on-smartparens-strict-mode)
 
   (use-package js2-refactor
     :config
