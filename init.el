927e70200936ba34dcd1e817d1ea650678606876
Matthew Batema <matthew.batema@gmail.com>
2018-05-14 21:27:03 -0700

    Time emacs startup.

--- init.el
+++ init.el
@@ -418,5 +418,13 @@ The following %-sequences are provided:
 
 (use-package restart-emacs)
 
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "Emacs ready in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
+
 (pushover-send "emacs"
                "startup complete")
