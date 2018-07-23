fd248adba41404806a33331a6b78ded5a6f1c186
Matthew Batema <matthew.batema@gmail.com>
2018-07-22 22:00:18 -0700

    LOCAL: home.

---
+++ custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("~/org/01_notes.org" "~/org/notes.org" "~/org/jobhunt.org" "~/org/bills.org" "~/org/fairway.org" "~/org/01_todo.org")))
+ '(org-log-into-drawer t)
+ '(org-log-state-notes-insert-after-drawers t)
+ '(package-selected-packages
+   (quote
+    (org-gcal dockerfile-mode lsp-ui company-lsp lsp-javacomp restart-emacs ace-window flycheck-elm elm-mode which-key fancy-battery fancy-batter highlight-numbers spaceline delight smartparens-config pushover helm-pass sky-color-clock go-mode flycheck-clojure org-journal docker clj-refactor alert n4js scala-mode maxframe yaml-mode undo-tree docker-tramp rainbow-identifiers gist graphviz-dot-mode company expand-region js2-refactor js2-mode rainbow-delimiters flycheck-pos-tip flycheck exec-path-from-shell auctex magit markdown-mode anzu cider idle-highlight-mode paredit helm-descbinds helm better-defaults super-save nyan-mode solarized-theme use-package)))
+ '(safe-local-variable-values (quote ((bug-reference-bug-regexp . "#\\(?2:[0-9]+\\)"))))
+ '(use-package-always-ensure t)
+ '(use-package-hook-name-suffix "")
+ '(use-package-verbose t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
