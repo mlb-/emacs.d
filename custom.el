c6fa098ed35f8004460ad178a6587736ea2d5a60
Matthew Batema <matthewbatema@vyncacare.com>
2024-07-02 22:04:58 -0700

    LOCAL: vynca

---
+++ custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(epg-pinentry-mode 'loopback nil nil "Customized with use-package epa")
+ '(package-selected-packages
+   '(copilot editorconfig quelpa-use-package quelpa magit-gitlab company-lsp yaml-mode which-key wgrep-helm undo-tree super-save spaceline solarized-theme smartparens restart-emacs rainbow-identifiers rainbow-delimiters pushover org-journal org-gcal nyan-mode maxframe magit lsp-ui lsp-javacomp js2-refactor idle-highlight-mode helm-projectile helm-pass helm-descbinds graphviz-dot-mode git-link gist flycheck-pos-tip flycheck-elm flycheck-clojure fancy-battery expand-region exec-path-from-shell elm-mode docker delight company clj-refactor better-defaults auctex anzu ace-window))
+ '(safe-local-variable-values
+   '((eval setq exec-path
+           (append exec-path
+                   '("~/Library/Caches/pypoetry/virtualenvs/vos-core-svc-AUke3mro-py3.12/bin")))
+     (projectile-project-test-cmd . "cd apps/vos-core-svc/ && poetry run pytest $(git ls-files -m vos_core tests)"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
